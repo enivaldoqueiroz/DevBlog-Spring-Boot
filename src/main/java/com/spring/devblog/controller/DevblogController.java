b06420761d9cbc1781dca7be6ddd981d05ce7ff4
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2020-10-04 22:47:48 -0300

    Criacao do Formulario para gerar novo Posts

--- src/main/java/com/spring/devblog/controller/DevblogController.java
+++ src/main/java/com/spring/devblog/controller/DevblogController.java
@@ -1,13 +1,18 @@
 package com.spring.devblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.devblog.model.Post;
 import com.spring.devblog.service.DevblogService;
@@ -33,5 +38,21 @@ public class DevblogController {
 		mv.addObject("post", post);
 		return mv;
 	}
+
+	@RequestMapping(value="/newpost", method=RequestMethod.GET)
+    public String getPostForm(){
+        return "postForm";
+    }
+
+    @RequestMapping(value="/newpost", method=RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        devblogService.save(post);
+        return "redirect:/posts";
+    }
 
 }
